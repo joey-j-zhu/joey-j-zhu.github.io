6fb4c51142a8b2e20fbb917c2cc8b11e6f6bfc22
Joey Zhu <joey.j.zhu@berkeley.edu>
2021-06-20 19:27:34 -0700

    Canvas is functioning

--- diffuse.js
+++ diffuse.js
@@ -1,6 +1,6 @@
 // DIFFUSE GRID
 
-var SIZE = [64, 64];
+var SIZE = [48, 27];
 var X = 0;
 var Y = 1;
 
@@ -8,6 +8,9 @@ function grid(c) {
     var a = new Array(c[Y]);
     for (var y = 0; y < SIZE[Y]; y++) {
         a[y] = new Array(c[X]);
+        for (var x = 0; x < SIZE[X]; x++) {
+            a[y][x] = 0;
+        }
     }
     return a;
 }
@@ -72,7 +75,7 @@ function transfer(arr, x1, y1, x2, y2, amt1, amt2) {
     arr[y2][x2] = cell2 + amt2 * (cell1 - cell2);
 }
 
-function step(arr, wFunc, d, diff, probWeight) {
+function step(arr, wFunc, d) {
     var x = 0;
     var y = 0;
     var finished = false;
@@ -98,22 +101,27 @@ var G0 = [0, 0];
 var G1 = [960, 540];
 var CELL_SIZE = [(G1[X] - G0[X]) / SIZE[X], (G1[Y] - G0[Y]) / SIZE[Y]];
 
-var MOUSE_BUFFER = 10;
-currentBuffer = 1;
+var MOUSE_BUFFER = 20;
+var currentBuffer = 1;
 
 function globalToLocal(c) {
     return [SIZE[X] * (c[X] - G0[X]) / (G1[X] - G0[X]), SIZE[Y] * (c[Y] - G0[Y]) / (G1[Y] - G0[Y])];
 }
+
+function localToGlobal(c) {
+    return [G0[X] + (G1[X] - G0[X]) * c[X] / SIZE[X], G0[Y] + (G1[Y] - G0[Y]) * c[Y] / SIZE[Y]];
+}
+
 var cursorStream = [globalToLocal(mouse)]
 
 var totalC = [mouse[X], mouse[Y]];
 var smoothC = [mouse[X], mouse[Y]];
 var prevC = globalToLocal([mouse[X], mouse[Y]]);
 
-var MOUSE_RANGE = [50, 50];
-var FEED_RATE = 100;
-var FADE_RATE = 0.2;
-var MOVE_RATE = 0.8
+var MOUSE_RANGE = [100, 100];
+var FEED_RATE = 0.8;
+var FADE_RATE = 0.8;
+var MOVE_RATE = 0.2;
 
 function heat(grid, c) {
     var ax = Math.max(0, c[X] - MOUSE_RANGE[X]);
@@ -122,28 +130,33 @@ function heat(grid, c) {
     var by = Math.min(SIZE[Y] - 1, (c[Y] + MOUSE_RANGE[Y]) + 1);
     for (var x = ax; x <= bx; x++) {
         for (var y = ay; y < by; y++) {
-            var dx = x - c[x];
-            var dy = y - c[y];
+            var dx = x - c[X];
+            var dy = y - c[Y];
             var r2 = dx * dx + dy * dy;
-            grid[y][x] += FEED_RATE * Math.random() / (r2 + 1);
+            grid[y][x] = grid[y][x] + FEED_RATE / (r2 + 1);
         }
     }
 }
 
 function addFrame(gc) {
-    var c = globalToLocal(gc);
-    cursorStream.push(c);
-
+    var c = gc;
+    // Add to top of stream
+    cursorStream.push([c[X], c[Y]]);
+    totalC[X] += c[X];
+    totalC[Y] += c[Y];
+    
     if (currentBuffer == MOUSE_BUFFER) {
-        var last = cursorStream.pop();
-        self.totalC[X] = self.totalC[X] - last[X];
-        self.totalC[Y] = self.totalC[Y] - last[Y];
+        // If stream is at size, take out bottom
+        var last = cursorStream.shift();
+        totalC[X] = totalC[X] - last[X];
+        totalC[Y] = totalC[Y] - last[Y];
     } else {
         currentBuffer++;
     }
     prevC = smoothC;
-    smoothC[X] = smoothC[X] + (totalC[X] / MOUSE_BUFFER - smoothC[X]) * MOVE_RATE;
-    smoothC[Y] = smoothC[Y] + (totalC[Y] / MOUSE_BUFFER - smoothC[Y]) * MOVE_RATE;
+    
+    smoothC[X] = smoothC[X] + (totalC[X] / currentBuffer - smoothC[X]) * MOVE_RATE;
+    smoothC[Y] = smoothC[Y] + (totalC[Y] / currentBuffer - smoothC[Y]) * MOVE_RATE;
 }
 
 function mouseV() {
@@ -152,6 +165,82 @@ function mouseV() {
 
 
 
-// MAIN
+
+// GRID BUFFER
+
+
+
+
+
+
+// CANVAS
+
+canvasSize = [G1[X] - G0[X], G1[Y] - G0[Y]];
+
+var BASE_COLOR = "#606060";
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
 
 var mainGrid = grid(SIZE[X], SIZE[Y]);
+
+var RAD = 5;
+var BASE = 2;
+
+function drawCircle(canvas, c, radius) {
+    canvas.beginPath();
+    canvas.arc(c[X], c[Y], radius, 0 , 2 * Math.PI);
+    canvas.fillStyle = BASE_COLOR;
+    canvas.fill();
+}
+
+function drawRect(canvas, c0, c1) {
+    canvas.beginPath();
+    canvas.rect(c0[X], c0[Y], c1[X], c1[Y]);
+    canvas.fillStyle = "white";
+    canvas.fill();
+    canvas.stroke();
+}
+
+function render(canvas, grid) {
+    var c;
+    for (var y = 0; y < SIZE[Y]; y++) {
+        for (var x = 0; x < SIZE[X]; x++) {
+            c = localToGlobal([x + 0.5, y + 0.5]);
+            drawCircle(canvas, c, BASE + RAD * grid[y][x]);
+        }
+    }
+}
+
+function update(grid) {
+    addFrame(mouse);
+    for (var i = 0; i < 100; i++) {
+        //step(grid, 3);
+    }
+
+    heat(grid, globalToLocal(smoothC));
+    for (var y = 0; y < SIZE[Y]; y++) {
+        for (var x = 0; x < SIZE[X]; x++) {
+            mainGrid[y][x] = mainGrid[y][x] * FADE_RATE;
+        }
+    }
+}
+
+
+
+// INTERFACE
+
+function mainloop() {
+    requestAnimationFrame(mainloop);
+    ctx.clearRect(0, 0, canvasSize[X], canvasSize[Y]);
+    
+    render(ctx, mainGrid);
+    update(mainGrid);
+}
+
+mainloop();
+
+window.addEventListener('mousemove', function(e) {
+    mouse[X] = e.x;
+    mouse[Y] = e.y;
+})
