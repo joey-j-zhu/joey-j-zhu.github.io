68d2b3f8b0f6f42b202c6c08ca8ac7e956c6c850
Joey Zhu <joey.j.zhu@berkeley.edu>
2023-02-19 21:22:57 -0800

    content and code is all up and functional. refactoring still needed but ready for deployment

--- src/components/contacts/calendar.js
+++ src/components/contacts/calendar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Calendar = (props) => {
     return (
         <div>
-            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%232d2a34&ctz=America%2FLos_Angeles&src=am9leS5qLnpodUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=OWE1NDUwMDA5ZjBkMDJiMDJlMTA2NDM2OTA2MzQzNjVlZTMwZGRmZGQ1ZGI3YmRlMjVkN2ZkYzkzMjRlYTY2ZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=am9leWp6aHVAbGJsLmdvdg&color=%232d2a34&color=%232d2a34&color=%232d2a34&color=%232d2a34" 
+            <iframe src="https://calendar.google.com/calendar/embed?src=cb1b3aa5f21d7c73f794af56d840108eac85142181d7f93901dfb0bb8889b684%40group.calendar.google.com&ctz=America%2FLos_Angeles" 
             style={{
                 border:"solid 0px #777", 
                 width:1100, 
